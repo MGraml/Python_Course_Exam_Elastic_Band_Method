031e78994fa2b0a8f4465450b35f1bfd244f1020
Max Graml <maximilian.graml@physik.uni-regensburg.de>
2021-08-01 12:16:41 +0200

    attempt on delivering raw data

--- el_band.py
+++ el_band.py
@@ -1,3 +1,16 @@
 import scipy as sp
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import time
+import random
+
+#data = pd.read_csv("./map_data.csv")
+
+x   = np.linspace(-10,10,1000)
+X,Y = np.meshgrid(x,x)
+Z   = 4*np.exp(-(X+Y)**2)+2*np.exp(-(X-random.randrange(-10,10))**2)
+
+
+plt.contourf(X,Y,Z)
+plt.savefig("map_raw.png")
