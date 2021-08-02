6848f333ea6cace04d97c4bd43facdab676a2000
Max Graml <maximilian.graml@physik.uni-regensburg.de>
2021-08-02 15:50:34 +0200

    Sucessful band implementation

--- el_band.py
+++ el_band.py
@@ -38,18 +38,19 @@ def mapCreation(offs, N = 1000, intv=(-10,10)):
     - 
     """
     
-    x   = np.linspace(intv,N)
+    x   = np.linspace(*intv,N)
     X,Y = np.meshgrid(x,x)
     Z = -1*np.exp(-(X-offs)**2)-1*np.exp(-(Y-offs)**2)
     Z = np.where(Z<-1,-1,Z)
 
     plt.contourf(X,Y,Z)
     plt.savefig("map_raw.png")
-    Z = pd.DataFrame(Z)
-    Z.to_csv("map_data.csv")
-    return Z
+    #data = pd.DataFrame(X,Y,Z)
+    #data.to_csv("map_data.csv")
+    np.save("data_raw",X,Y,Z)
+    return X,Y,Z
 
-def Energy(band,k=1):
+def Energy(band,init,final,k=1):
     """
     These are both energies: the elastic band part, giving an energy contribution via Hooke's law and the potential energy/height.
     Necessary arguments:
@@ -58,19 +59,28 @@ def Energy(band,k=1):
     Optional arguments:
     - k     the spring constant, by default set to 1
     """
+    print(init,final)
     #band = np.array(band)
     #print(len(band))
-    X = band[::2]
-    Y = band[1::2]
+    X = list(band[::2])
+    X.insert(0,init[0])
+    X.append(final[0])
+    Y = list(band[1::2])
+    Y.insert(0,init[1])
+    Y.append(final[1])
+    print(f'X={X}')
+    print(f'Y={Y}')
     #X = [band[i][0] for i in range(len(band))]
     #Y = [band[i][1] for i in range(len(band))]
     results = []
-    for idpoint in range(len(band)//2):
-        if idpoint == 0 or idpoint == len(band)//2-1:
-            results.append(0)
+    for idpoint in range(len(X)):
+        if idpoint == 0:
+            results.append(k*((X[0]-X[1])**2+(Y[0]-Y[1])**2))
+        elif idpoint == len(X)-1:
+            results.append(k*((X[-1]-X[-2])**2+(Y[-1]-Y[-2])**2))
         else:
             results.append(k * ((X[idpoint-1]-X[idpoint])**2+(X[idpoint+1]-X[idpoint])**2 + \
-                        (Y[idpoint-1]-Y[idpoint])**2 + (Y[idpoint+1]-Y[idpoint-1])**2))
+                        (Y[idpoint-1]-Y[idpoint])**2 + (Y[idpoint+1]-Y[idpoint])**2))
     return np.sum(results)
     
 
@@ -79,7 +89,7 @@ def Energy(band,k=1):
 
 offs = -3*10/4
 #giving the length of the band N and the initial/final endpoint coordinates p
-N_band  = 5
+N_band  = 10
 p_init  = (10,offs)
 p_final = (offs,10)
 #Creating a linear interpolation between the two points as a first guess
@@ -88,14 +98,18 @@ band[:,0]   = np.linspace(p_init[0],p_final[0],N_band)
 band[:,1]   = np.linspace(p_init[1],p_final[1],N_band)
 band_list = [band[i,:] for i in range(np.shape(band)[0])]
 #print(band)
-data = pd.read_csv("map_data.csv")
+#data = pd.read_csv("map_data.csv")
+X,Y,Z = mapCreation(offs)
 
+res = opt.minimize(Energy,band_list[1:-1],args=(band_list[0],band_list[-1]))#,options={'maxiter' : 10})
 
-res = opt.minimize(Energy,band_list)
 #%%
 new_x = np.array(res.x[::2])
 new_y = np.array(res.x[1::2])
 new_band = np.stack((new_x,new_y))
-plt.contourf(data)
-plt.scatter(new_band[0],new_band[1])
 print(new_band)
+plt.contourf(X,Y,Z)
+plt.scatter(new_band[0],new_band[1])
+plt.scatter(band[:,0],band[:,1],c='red',s=1)
+plt.show()
+
