580f096d0278db7353524e8c985cab4d7af8b9cd
Max Graml <maximilian.graml@physik.uni-regensburg.de>
2021-08-02 17:30:47 +0200

    Indices for band contribution okay, moving on to implementing height

--- el_band.py
+++ el_band.py
@@ -48,9 +48,9 @@ def mapCreation(offs, N = 1000, intv=(-10,10)):
     #data = pd.DataFrame(X,Y,Z)
     #data.to_csv("map_data.csv")
     np.save("data_raw",X,Y,Z)
-    return X,Y,Z
+    return x,x,Z
 
-def Energy(band,init,final,k=1):
+def Energy(band,init,final,x_ext,y_ext,Z_ext,k=1):
     """
     These are both energies: the elastic band part, giving an energy contribution via Hooke's law and the potential energy/height.
     Necessary arguments:
@@ -69,11 +69,12 @@ def Energy(band,init,final,k=1):
     Y.insert(0,init[1])
     Y.append(final[1])
     
-    print(f'X={X}')
-    print(f'Y={Y}')
+    #print(f'X={X}')
+    #print(f'Y={Y}')
     
-    X = [np.round(x) for x in X]
-    Y = [np.round(y) for y in Y]
+    X = [x_ext[int(np.round(x))] for x in X]
+    Y = [y_ext[int(np.round(y))] for y in Y]
+
     
     #X = [band[i][0] for i in range(len(band))]
     #Y = [band[i][1] for i in range(len(band))]
@@ -91,31 +92,41 @@ def Energy(band,init,final,k=1):
 
 
 
-
+N=1000
 offs = -3*10/4
-#giving the length of the band N and the initial/final endpoint coordinates p
+#giving the length of the band N and the initial/final endpoint coordinates p as INDICES
 N_band  = 10
-p_init  = (10,offs)
-p_final = (offs,10)
+p_init  = (999,125)
+p_final = (125,999)
+
+x,y,Z = mapCreation(offs,N=N)
+
+print(p_init[0],p_final[0])
 #Creating a linear interpolation between the two points as a first guess
 band = np.zeros([N_band,2])
-band[:,0]   = np.arange(0,10000,10000//N_band)
-band[:,1]   = np.arange(0,10000,10000//N_band)
-band[3,0]   = -600
+band[:,0]   = np.linspace(p_init[0],p_final[0],N_band)
+band[:,1]   = np.linspace(p_init[1],p_final[1],N_band)
+#band[3,0]   = 600
 band_list = [band[i,:] for i in range(np.shape(band)[0])]
 #print(band)
 #data = pd.read_csv("map_data.csv")
-X,Y,Z = mapCreation(offs)
 
-res = opt.minimize(Energy,band_list[1:-1],args=(band_list[0],band_list[-1]),method = 'BFGS',options={'maxiter' : 20, 'eps' : 1.0})
+
+res = opt.minimize(Energy,band_list[1:-1],args=(band_list[0],band_list[-1],x,y,Z),method = 'BFGS',options={'maxiter' : 20, 'eps' : 2.0})
 
 #%%
 new_x = np.array(res.x[::2])
 new_y = np.array(res.x[1::2])
 new_band = np.stack((new_x,new_y))
 print(new_band)
-plt.contourf(X,Y,Z)
-plt.scatter(new_band[0],new_band[1])
-plt.scatter(band[:,0],band[:,1],c='red',s=1)
+plt.contourf(x,y,Z)
+x_init = [x[int(round(i))] for i in band[:,0]]
+y_init = [y[int(round(i))] for i in band[:,1]]
+plt.scatter(x_init,y_init)
+#print(x[int(round(new_band[2,0]))])
+x_final = [x[int(round(i))] for i in new_band[0,:]]
+y_final = [y[int(round(i))] for i in new_band[1,:]]
+#print(x_final,y_final,np.shape(x_final))
+plt.scatter(x_final,y_final,c='red',s=1)
 plt.show()
 
