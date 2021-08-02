de0ffce40913019f20c86fb99c96044b7b12e8e4
Max Graml <maximilian.graml@physik.uni-regensburg.de>
2021-08-02 11:43:53 +0200

    added taketime and mapcreation function (csv)

--- el_band.py
+++ el_band.py
@@ -5,12 +5,30 @@ import pandas as pd
 import time
 import random
 
-#data = pd.read_csv("./map_data.csv")
-
-x   = np.linspace(-10,10,1000)
-X,Y = np.meshgrid(x,x)
-Z   = 4*np.exp(-(X+Y)**2)+2*np.exp(-(X-random.randrange(-10,10))**2)
 
 
-plt.contourf(X,Y,Z)
-plt.savefig("map_raw.png")
+def takeTime (func) :
+    def wrapper (*args, **kwargs) :
+        tic = time.perf_counter()
+        result = func(*args, **kwargs)
+        toc = time.perf_counter()
+        print(f"Time elapsed: {(toc - tic) * 1000:8.3f} ms")
+        return result
+    return wrapper
+
+def mapCreation():
+    N = 1000
+    d = 10
+    offs = -3*d/4
+    x   = np.linspace(-d,d,N)
+    X,Y = np.meshgrid(x,x)
+    Z = -1*np.exp(-(X-offs)**2)-1*np.exp(-(Y-offs)**2)
+    Z = np.where(Z<-1,-1,Z)
+
+    plt.contourf(X,Y,Z)
+    plt.savefig("map_raw.png")
+    Z = pd.DataFrame(Z)
+    Z.to_csv("map_data.csv")
+    return Z
+
+data = pd.read_csv("./map_data.csv")
