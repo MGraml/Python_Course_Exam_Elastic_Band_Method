6edc6571251265c5e26a68670bd8f769329f6c6f
Max Graml <maximilian.graml@physik.uni-regensburg.de>
2021-08-03 14:56:50 +0200

    Its working!

--- el_band.py
+++ el_band.py
@@ -40,7 +40,7 @@ def mapCreation(offs, N = 10000, intv=(-10,10)):
     
     x   = np.linspace(*intv,N)
     X,Y = np.meshgrid(x,x)
-    Z = -1*np.exp(-(X-offs)**2)-1*np.exp(-(Y-offs)**2)+np.exp(-1/3*(X+offs)**2)+3*np.exp(-0.01*(Y)**2)
+    Z = -1*np.exp(-(X-offs)**2)-1*np.exp(-(Y-offs)**2)#-1*np.exp(-(X-offs)**2)-1*np.exp(-(Y-offs)**2)+np.exp(-1/3*(X+offs)**2)+3*np.exp(-0.01*(Y)**2)
     Z = np.where(Z<-1,-1,Z)
 
     plt.contourf(X,Y,Z)
@@ -50,7 +50,7 @@ def mapCreation(offs, N = 10000, intv=(-10,10)):
     np.save("data_raw",X,Y,Z)
     return x,x,Z
 
-def Energy(band,init,final,x_ext,y_ext,Z,N=1000,k=1e-3):
+def Energy(band,init,final,x_ext,y_ext,Z,N=1000,k=1):
     """
     These are both energies: the elastic band part, giving an energy contribution via Hooke's law and the potential energy/height.
     Necessary arguments:
@@ -72,7 +72,7 @@ def Energy(band,init,final,x_ext,y_ext,Z,N=1000,k=1e-3):
     idx_y = []
     #Creating Indices for Coordinates in Height
     #print(x_ext)
-    spacing = (max(x_ext)-min(x_ext))/1000
+    spacing = (max(x_ext)-min(x_ext))/N
 
     for x in X:
         comp_x = np.argwhere(np.where(np.abs(x_ext-x)<0.9*spacing,1,0))
@@ -110,29 +110,31 @@ def Energy(band,init,final,x_ext,y_ext,Z,N=1000,k=1e-3):
         else:
             results.append(k * ((X[idpoint-1]-X[idpoint])**2+(X[idpoint+1]-X[idpoint])**2 + \
                         (Y[idpoint-1]-Y[idpoint])**2 + (Y[idpoint+1]-Y[idpoint])**2))
-    print(np.sum(results),poten)
-    return poten
+    #print(np.sum(results),poten)
+    return poten + np.sum(results)
     
 
 
 
-N=10000
+N=5000
 offs = -3*10/4
 #giving the length of the band N and the initial/final endpoint coordinates p
-N_band  = 10
+N_band  = 50
 p_init  = (10,offs)
 p_final = (offs,10)
 #Creating a linear interpolation between the two points as a first guess
 band = np.zeros([N_band,2])
 band[:,0]   = np.linspace(p_init[0],p_final[0],N_band)
 band[:,1]   = np.linspace(p_init[1],p_final[1],N_band)
-band[4,0]   = -offs-0.5
+#band[4,0]   = +offs+0.5
 band_list = [band[i,:] for i in range(np.shape(band)[0])]
 #print(band)
 #data = pd.read_csv("map_data.csv")
 x,y,Z = mapCreation(offs,N=N)
 
-res = opt.minimize(Energy,band_list[1:-1],args=(band_list[0],band_list[-1],x,y,Z,N),options={'disp':True})
+bound = [(-10,10) for _ in range((N_band-2)*2)]
+spacing = (max(x)-min(x))/N
+res = opt.minimize(Energy,band_list[1:-1],args=(band_list[0],band_list[-1],x,y,Z,N),bounds=bound,jac='2-point',options={'disp':True,'finite_diff_rel_step':spacing})
 
 #%%
 new_x = np.array(res.x[::2])
@@ -140,7 +142,8 @@ new_y = np.array(res.x[1::2])
 new_band = np.stack((new_x,new_y))
 print(new_band)
 X,Y = np.meshgrid(x,y)
-plt.contourf(X,Y,Z)
+plt.contourf(X,Y,Z,levels=25,colormap='gist_heat')
+plt.colorbar()
 plt.scatter(new_band[0],new_band[1],c='green')
 plt.scatter(band[:,0],band[:,1],c='red',s=1)
 plt.show()
